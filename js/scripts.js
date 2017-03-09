04e768e8b404528d513541caa8ba70e6c0555220
Iza M <imalnowicz@gmail.com>
2017-03-09 12:18:56 +0100

    tablice

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Marian';
+
+var s = allNames.indexOf(newName) === -1
+
+console.log(s);
+
+allNames.push(newName);
+
+console.log(allNames);
+
+
+
+
+
+
